192a826b65dca3a4bb07326b8f805c94d4cce309
Youngjoon Park <pyj2025@gmail.com>
2024-01-10 22:55:06 -0800

    initial home screen

--- App.js
+++ App.js
@@ -1,22 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './components/Home';
+import Test from './components/Test';
+import Ggetmal from './components/Ggetmal';
+import Gugudan from './components/Gugudan';
+import NumberBaseball from './components/NumberBaseball';
+import ResponseCheck from './components/ResponseCheck';
+import Rsp from './components/Rsp';
+import Lotto from './components/Lotto';
+import TicTacToe from './components/TicTacToe';
+import MineSearch from './components/MineSearch';
+import { Button } from 'react-native';
+import { HeaderBackButton } from '@react-navigation/stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-        <StatusBar style="auto" />
-        <Home />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Ggetmal" component={Ggetmal} />
+        <Stack.Screen name="Gugudan" component={Gugudan} />
+        <Stack.Screen name="Lotto" component={Lotto} />
+        <Stack.Screen name="NumberBaseball" component={NumberBaseball} />
+        <Stack.Screen name="ResponseCheck" component={ResponseCheck} />
+        <Stack.Screen name="Rsp" component={Rsp} />
+        <Stack.Screen name="TicTacToe" component={TicTacToe} />
+        <Stack.Screen name="MineSearch" component={MineSearch} />
+        <Stack.Screen name="Test" component={Test} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
